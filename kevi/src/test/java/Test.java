276a0d42ffacd0f2e67621213a1fc14aa6b24032
qq23400262 <23400262@qq.com>
2016-01-30 16:45:36 +0800

    test

---
+++ kevi/src/test/java/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+	volatile String abc = "";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(10 & (15-1));
+		
+		System.out.println(1009<<32);
+	}
+
+}

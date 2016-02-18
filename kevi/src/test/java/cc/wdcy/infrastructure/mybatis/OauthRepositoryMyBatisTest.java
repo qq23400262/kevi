aca4e26621aa0b8a9aaecf5346bc9063b8b5b97d
qq23400262 <23400262@qq.com>
2016-02-18 16:09:26 +0800

    first commit

--- kevi/src/test/java/cc/wdcy/infrastructure/mybatis/OauthRepositoryMyBatisTest.java
+++ kevi/src/test/java/cc/wdcy/infrastructure/mybatis/OauthRepositoryMyBatisTest.java
@@ -17,43 +17,43 @@ import static org.testng.Assert.*;
 public class OauthRepositoryMyBatisTest extends AbstractRepositoryTest {
 
 
-    @Autowired
-    private OauthRepository oauthRepositoryMyBatis;
-
-
-    @Test
-    public void findOauthClientDetails() {
-        OauthClientDetails oauthClientDetails = oauthRepositoryMyBatis.findOauthClientDetails("unity-client");
-        assertNull(oauthClientDetails);
-
-    }
-
-
-    @Test
-    public void saveOauthClientDetails() {
-
-        final String clientId = GuidGenerator.generate();
-
-        OauthClientDetails clientDetails = new OauthClientDetails().clientId(clientId);
-        oauthRepositoryMyBatis.saveOauthClientDetails(clientDetails);
-
-        final OauthClientDetails oauthClientDetails = oauthRepositoryMyBatis.findOauthClientDetails(clientId);
-        assertNotNull(oauthClientDetails);
-        assertNotNull(oauthClientDetails.clientId());
-        assertNull(oauthClientDetails.clientSecret());
-
-    }
-
-    @Test
-    public void findAllOauthClientDetails() {
-        final List<OauthClientDetails> allOauthClientDetails = oauthRepositoryMyBatis.findAllOauthClientDetails();
-        assertNotNull(allOauthClientDetails);
-        assertTrue(allOauthClientDetails.isEmpty());
-    }
-
-    @Test
-    public void updateOauthClientDetailsArchive() {
-        oauthRepositoryMyBatis.updateOauthClientDetailsArchive("ddooelddd", true);
-    }
+//    @Autowired
+//    private OauthRepository oauthRepositoryMyBatis;
+//
+//
+//    @Test
+//    public void findOauthClientDetails() {
+//        OauthClientDetails oauthClientDetails = oauthRepositoryMyBatis.findOauthClientDetails("unity-client");
+//        assertNull(oauthClientDetails);
+//
+//    }
+//
+//
+//    @Test
+//    public void saveOauthClientDetails() {
+//
+//        final String clientId = GuidGenerator.generate();
+//
+//        OauthClientDetails clientDetails = new OauthClientDetails().clientId(clientId);
+//        oauthRepositoryMyBatis.saveOauthClientDetails(clientDetails);
+//
+//        final OauthClientDetails oauthClientDetails = oauthRepositoryMyBatis.findOauthClientDetails(clientId);
+//        assertNotNull(oauthClientDetails);
+//        assertNotNull(oauthClientDetails.clientId());
+//        assertNull(oauthClientDetails.clientSecret());
+//
+//    }
+//
+//    @Test
+//    public void findAllOauthClientDetails() {
+//        final List<OauthClientDetails> allOauthClientDetails = oauthRepositoryMyBatis.findAllOauthClientDetails();
+//        assertNotNull(allOauthClientDetails);
+//        assertTrue(allOauthClientDetails.isEmpty());
+//    }
+//
+//    @Test
+//    public void updateOauthClientDetailsArchive() {
+//        oauthRepositoryMyBatis.updateOauthClientDetailsArchive("ddooelddd", true);
+//    }
 
 }
